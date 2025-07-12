f6d6b973ecab442b676f23f3ea80a0a42c46fe28
Tushar Desai <imtushardesai@gmail.com>
2025-07-12 13:35:21 +0530

    landing page done

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import "./App.css";
+import Landing from "./pages/Landing";
+
+function AppContent() {
+  const location = useLocation();
+  // Always enable dark mode for the app
+  React.useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
+  return (
+    <>
+      {/* Navbar is included inside LandingPage and hidden on /login, /signup */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        {/* Add more routes here */}
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
